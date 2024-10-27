ec24a7cac1810962269b817f1a266359b66f50e5
Oliver Binns <mail@oliverbinns.co.uk>
2024-10-27 14:01:42 +0000

    feat: decoded additional parameters in AttestationObject (#7)

--- AppAttest/Sources/AttestationDecoder/AttestationObject.swift
+++ AppAttest/Sources/AttestationDecoder/AttestationObject.swift
@@ -2,8 +2,12 @@ import Foundation
 
 public struct AttestationObject: Decodable {
     public let format: String
+    public let authenticatorData: Data
+    public let statement: AttestationStatement
 
     enum CodingKeys: String, CodingKey {
         case format = "fmt"
+        case authenticatorData = "authData"
+        case statement = "attStmt"
     }
 }
