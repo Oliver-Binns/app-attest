7adada9419c087d7b87179f13e66eb2036979eb9
Oliver Binns <mail@oliverbinns.co.uk>
2025-10-04 21:01:29 +0100

    feat: generate assertion object (#29)
    * feat: generate assertion object
    
    * docs: updated completion progress in README.md

--- Server/Sources/Server/configure.swift
+++ Server/Sources/Server/configure.swift
@@ -7,10 +7,6 @@ import Vapor
 public func configure(_ app: Application) async throws {
     app.databases.use(DatabaseConfigurationFactory.sqlite(.file("db.sqlite")), as: .sqlite)
     app.migrations.add(CreateAppInstance())
-    app.middleware.use(ClientAttestationMiddleware(
-        appID: "Z86DH46P79.uk.co.oliverbinns.app-attest",
-        environment: .development
-    ))
     // register routes
     try routes(app)
 }
